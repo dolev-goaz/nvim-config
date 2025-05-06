b15148e77b1ceb08da27fb7b466a15cb812e25e2
dolev <www.dolev@gmail.com>
2025-05-06 23:45:09 +0300

    don't use absolute paths

--- lua/clipboard.lua
+++ lua/clipboard.lua
@@ -1,6 +1,10 @@
 if vim.fn.has("wsl") then
-	-- if ran on my main PC- use win32yank
-	local clipboard_path = "/mnt/c/programdata/chocolatey/lib/win32yank/tools/win32yank.exe"
+    -- if running through WSL, use wind32yank to communicate with windows clipboard
+	local clipboard_path = vim.fn.exepath("win32yank.exe")
+	if clipboard_path == "" then
+		vim.notify("win32yank.exe not found in PATH. Clipboard integration in WSL skipped.", vim.log.levels.WARN)
+		return
+	end
 	vim.g.clipboard = {
 		name = "wslclipboard",
 		copy = {
