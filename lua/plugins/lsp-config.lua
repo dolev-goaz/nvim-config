e4f425984cd46e4793c356ff63ecc86fe36b718c
dolevgo <www.dolev@gmail.com>
2025-05-07 15:24:40 +0300

    lsp config options

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -55,6 +55,10 @@ return {
             lspconfig["volar"].setup({ capabilities = blink_capabilities })
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
