6fae43fb810798452fc5922d139e1c660ad2567e
dolev <www.dolev@gmail.com>
2025-05-08 13:01:40 +0300

    config neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -10,15 +10,22 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-        require("neo-tree").setup({
-            close_if_last_window = true,
-            filesystem = {
-                hijack_netrw_behavior = "open_default"
-            },
-            window = {
-                position = "right"
-            }
-        })
+		require("neo-tree").setup({
+			close_if_last_window = true,
+			filesystem = {
+				hijack_netrw_behavior = "open_default",
+				follow_current_file = {
+					enabled = true,
+					leave_dirs_open = false,
+				},
+                filtered_items = {
+                    always_show = { ".env" }
+                }
+			},
+			window = {
+				position = "right",
+			},
+		})
 		vim.keymap.set("n", "<C-e>", ":Neotree filesystem toggle<CR>", { silent = true })
 	end,
 }
