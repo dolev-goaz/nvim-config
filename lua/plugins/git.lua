8664f6278d8bebde8be268a0d562425f3f1d1036
dolev <www.dolev@gmail.com>
2025-05-08 22:28:38 +0300

    added fugitive, keybinds and updated

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -3,10 +3,14 @@ return {
         "lewis6991/gitsigns.nvim",
         config = function()
             require("gitsigns").setup({})
-            -- Open Git Preview
             vim.keymap.set("n", "<leader>ogp", ":Gitsigns preview_hunk<CR>", { desc = "Preview Git Hunk" })
-            -- Toggle Git Blame
             vim.keymap.set("n", "<leader>tgb", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle Git Blame" })
+            vim.keymap.set("n", "<leader>ogl", ":Telescope git_commits<CR>", { desc = "Open Git Logs" })
+            vim.keymap.set("n", "<leader>oglb", ":Telescope git_bcommits<CR>", { desc = "Open Git Logs in the current buffer" })
+            vim.keymap.set("n", "<leader>ogs", ":Telescope git_stash<CR>", { desc = "Open Git Stash" })
         end,
     },
+    {
+        "tpope/vim-fugitive",
+    }
 }
