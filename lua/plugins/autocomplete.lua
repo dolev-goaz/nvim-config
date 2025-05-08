148d78c90c138a032d1e7216ad2cc4453ded69a1
dolev <www.dolev@gmail.com>
2025-05-08 20:36:24 +0300

    added copilot

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,31 +1,36 @@
 return {
-    {
-        "saghen/blink.cmp",
-        dependencies = { "rafamadriz/friendly-snippets" },
-        version = "1.*",
-        opts = {
-            keymap = { preset = "default" },
-            appearance = {
-                nerd_font_variant = "mono",
-            },
-            completion = {
-                documentation = {
-                    auto_show = true,
-                    auto_show_delay_ms = 150,
-                },
-                ghost_text = {
-                    enabled = true,
-                },
-            },
-            signature = {
-                enabled = true,
-                -- show_on_insert = true,
-            },
-            sources = {
-                default = { "lsp", "path", "snippets", "buffer" },
-            },
-            fuzzy = { implementation = "prefer_rust_with_warning" },
-        },
-        opts_extend = { "sources.default" }
-    },
+	{
+		"saghen/blink.cmp",
+		dependencies = { "rafamadriz/friendly-snippets" },
+		version = "1.*",
+		opts = {
+			keymap = { preset = "default" },
+			appearance = {
+				nerd_font_variant = "mono",
+			},
+			completion = {
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 150,
+				},
+				ghost_text = {
+					enabled = true,
+				},
+			},
+			signature = {
+				enabled = true,
+				-- show_on_insert = true,
+			},
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+		},
+		opts_extend = { "sources.default" },
+	},
+	{
+        -- Setup with :Copilot setup
+        -- Get status with :Copilot status
+		"github/copilot.vim",
+	},
 }
