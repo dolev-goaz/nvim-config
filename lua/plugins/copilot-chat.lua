2d9aa2d8da3b6bc8e294c7832c633a9591f1336a
dolev <www.dolev@gmail.com>
2025-05-09 17:29:26 +0300

    Integration for github copilot agent

---
+++ lua/plugins/copilot-chat.lua
@@ -0,0 +1,27 @@
+local is_copilot_open = false
+local function toggle_copilot_chat()
+    local copilot_chat = require("CopilotChat")
+    if is_copilot_open then
+        copilot_chat.close()
+    else
+        copilot_chat.open({
+            context = { "files" },
+        })
+    end
+    is_copilot_open = not is_copilot_open
+end
+
+return {
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        dependencies = {
+            { "github/copilot.vim" },
+            { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+        },
+        build = "make tiktoken",
+        config = function()
+            require("CopilotChat").setup({})
+            vim.keymap.set("n", "<C-c>", toggle_copilot_chat, { desc = "Toggle Copilot Chat" })
+        end,
+    },
+}
