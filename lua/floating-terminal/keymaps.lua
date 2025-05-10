446ceca2e34e3bd5cff5e7e2c0d21fc587acf637
dolev <www.dolev@gmail.com>
2025-05-10 22:38:18 +0300

    changed remaps

--- lua/floating-terminal/keymaps.lua
+++ lua/floating-terminal/keymaps.lua
@@ -4,15 +4,15 @@ local M = {}
 
 function M.set_terminal_keymaps(enable)
     if enable then
-        vim.keymap.set({ "n", "t", "i" }, "<C-t><C-a>", tabs.new_tab, { desc = "Create new terminal tab" })
-        vim.keymap.set({ "n", "t", "i" }, "<C-t><C-n>", tabs.next_tab, { desc = "Next terminal tab" })
-        vim.keymap.set({ "n", "t", "i" }, "<C-t><C-p>", tabs.prev_tab, { desc = "Previous terminal tab" })
+        vim.keymap.set({ "n", "t", "i" }, "<C-t><C-n>", tabs.new_tab, { desc = "Create new terminal tab" })
+        vim.keymap.set({ "n", "t", "i" }, "<C-t><C-l>", tabs.next_tab, { desc = "Next terminal tab" })
+        vim.keymap.set({ "n", "t", "i" }, "<C-t><C-h>", tabs.prev_tab, { desc = "Previous terminal tab" })
         vim.keymap.set({ "n", "t", "i" }, "<C-t><C-w>", tabs.close_tab, { desc = "Close current terminal tab" })
         vim.keymap.set({ "n", "t", "i" }, "<C-t><C-s>", tabs.set_current_tab_title, { desc = "Set current tab title" })
     else
-        vim.keymap.del({ "n", "t", "i" }, "<C-t><C-a>")
         vim.keymap.del({ "n", "t", "i" }, "<C-t><C-n>")
-        vim.keymap.del({ "n", "t", "i" }, "<C-t><C-p>")
+        vim.keymap.del({ "n", "t", "i" }, "<C-t><C-l>")
+        vim.keymap.del({ "n", "t", "i" }, "<C-t><C-h>")
         vim.keymap.del({ "n", "t", "i" }, "<C-t><C-w>")
         vim.keymap.del({ "n", "t", "i" }, "<C-t><C-s>")
     end
