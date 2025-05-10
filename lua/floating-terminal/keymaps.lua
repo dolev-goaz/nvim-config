34569495c4ae13f80e72c99526bcce93c54aa0e3
dolev <www.dolev@gmail.com>
2025-05-10 16:00:52 +0300

    Restored set tab title

--- lua/floating-terminal/keymaps.lua
+++ lua/floating-terminal/keymaps.lua
@@ -3,17 +3,19 @@ local tabs = require("floating-terminal.tabs")
 local M = {}
 
 function M.set_terminal_keymaps(enable)
-	if enable then
-		vim.keymap.set({ "n", "t", "i" }, "<C-t><C-a>", tabs.new_tab, { desc = "Create new terminal tab" })
-		vim.keymap.set({ "n", "t", "i" }, "<C-t><C-n>", tabs.next_tab, { desc = "Next terminal tab" })
-		vim.keymap.set({ "n", "t", "i" }, "<C-t><C-p>", tabs.prev_tab, { desc = "Previous terminal tab" })
-		vim.keymap.set({ "n", "t", "i" }, "<C-t><C-w>", tabs.close_tab, { desc = "Close current terminal tab" })
-	else
-		vim.keymap.del({ "n", "t", "i" }, "<C-t><C-a>")
-		vim.keymap.del({ "n", "t", "i" }, "<C-t><C-n>")
-		vim.keymap.del({ "n", "t", "i" }, "<C-t><C-p>")
-		vim.keymap.del({ "n", "t", "i" }, "<C-t><C-w>")
-	end
+    if enable then
+        vim.keymap.set({ "n", "t", "i" }, "<C-t><C-a>", tabs.new_tab, { desc = "Create new terminal tab" })
+        vim.keymap.set({ "n", "t", "i" }, "<C-t><C-n>", tabs.next_tab, { desc = "Next terminal tab" })
+        vim.keymap.set({ "n", "t", "i" }, "<C-t><C-p>", tabs.prev_tab, { desc = "Previous terminal tab" })
+        vim.keymap.set({ "n", "t", "i" }, "<C-t><C-w>", tabs.close_tab, { desc = "Close current terminal tab" })
+        vim.keymap.set({ "n", "t", "i" }, "<C-t><C-s>", tabs.set_current_tab_title, { desc = "Set current tab title" })
+    else
+        vim.keymap.del({ "n", "t", "i" }, "<C-t><C-a>")
+        vim.keymap.del({ "n", "t", "i" }, "<C-t><C-n>")
+        vim.keymap.del({ "n", "t", "i" }, "<C-t><C-p>")
+        vim.keymap.del({ "n", "t", "i" }, "<C-t><C-w>")
+        vim.keymap.del({ "n", "t", "i" }, "<C-t><C-s>")
+    end
 end
 
 return M
--- lua/floating-terminal/tabs.lua
+++ lua/floating-terminal/tabs.lua
@@ -85,5 +85,85 @@ function M.close_tabline()
 	state.floating.tabline.win = -1
 end
 
+local function set_tab_title(tab_index)
+	if not state.floating.tabs[tab_index] then
+		return
+	end
+
+	-- Prevent multiple input windows
+	if state.floating.title_change.win and vim.api.nvim_win_is_valid(state.floating.title_change.win) then
+		return
+	end
+
+	-- Create a floating input window
+	local input_buf = vim.api.nvim_create_buf(false, true)
+	local width = 30
+	local height = 1
+	local col = math.floor((vim.o.columns - width) / 2)
+	local row = math.floor((vim.o.lines - height) / 2)
+
+	state.floating.title_change.win = vim.api.nvim_open_win(input_buf, true, {
+		relative = "editor",
+		width = width,
+		height = height,
+		col = col,
+		row = row,
+		style = "minimal",
+		border = "rounded",
+		title = "Set Tab Title",
+		title_pos = "center",
+	})
+
+	local augroup = vim.api.nvim_create_augroup("FloatingTerminalInput", { clear = true })
+	vim.api.nvim_create_autocmd("WinClosed", {
+		group = augroup,
+		pattern = tostring(state.floating.win),
+		callback = function()
+			if vim.api.nvim_win_is_valid(state.floating.title_change.win) then
+				vim.api.nvim_win_close(state.floating.title_change.win, true)
+				vim.api.nvim_buf_delete(input_buf, { force = true })
+			end
+			vim.api.nvim_del_augroup_by_id(augroup)
+		end,
+	})
+
+	-- TODO: make these keymaps work in both normal and insert mode
+	-- Set up keymaps for confirming or canceling the input
+	vim.api.nvim_buf_set_keymap(input_buf, "i", "<CR>", "", {
+		callback = function()
+			local lines = vim.api.nvim_buf_get_lines(input_buf, 0, 1, false)
+			local new_title = lines[1]
+			if new_title == "" then
+				new_title = "Tab " .. tab_index
+			end
+			state.floating.tabs[tab_index].title = new_title
+			vim.api.nvim_win_close(state.floating.title_change.win, true)
+			vim.api.nvim_buf_delete(input_buf, { force = true })
+			state.floating.title_change.win = nil -- Reset the active input window reference
+			vim.api.nvim_del_augroup_by_id(augroup) -- Clear the autocommand group
+			M.render_tabline()
+		end,
+		noremap = true,
+		silent = true,
+	})
+
+	vim.api.nvim_buf_set_keymap(input_buf, "n", "<Esc>", "", {
+		callback = function()
+			vim.api.nvim_win_close(state.floating.title_change.win, true)
+			vim.api.nvim_buf_delete(input_buf, { force = true })
+			state.floating.title_change.win = nil -- Reset the active input window reference
+			vim.api.nvim_del_augroup_by_id(augroup) -- Clear the autocommand group
+		end,
+		noremap = true,
+		silent = true,
+	})
+
+	-- vim.bo[input_buf].buftype = "prompt"
+	vim.cmd.startinsert()
+end
+
+function M.set_current_tab_title()
+	return set_tab_title(state.floating.current_tab)
+end
 
 return M
