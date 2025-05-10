940e1f84f2799217f8058c7959e6baa8667b5b25
dolev <www.dolev@gmail.com>
2025-05-11 00:32:12 +0300

    added key

--- lua/plugins/rename.lua
+++ lua/plugins/rename.lua
@@ -1,10 +1,11 @@
 return {
     "smjonas/inc-rename.nvim",
+    keys = { { "<leader>cr" } },
     config = function()
         require("inc_rename").setup({
             preview_empty_name = true,
             show_message = true,
-            save_in_cmdline_history = false
+            save_in_cmdline_history = false,
         })
         vim.keymap.set("n", "<leader>cr", ":IncRename ", { desc = "Rename Token" })
     end,
