83cb373eadc017c488a7e3c61e55c78f746dfd19
dolev <www.dolev@gmail.com>
2025-05-15 13:29:26 +0300

    added closing completion

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,37 +1,54 @@
 return {
-	{
-		"saghen/blink.cmp",
+    {
+        "saghen/blink.cmp",
         event = "InsertEnter",
-		dependencies = { "rafamadriz/friendly-snippets" },
-		version = "1.*",
-		opts = {
-			keymap = { preset = "default" },
-			appearance = {
-				nerd_font_variant = "mono",
-			},
-			completion = {
-				documentation = {
-					auto_show = true,
-					auto_show_delay_ms = 150,
-				},
-				ghost_text = {
-					enabled = true,
-				},
-			},
-			signature = {
-				enabled = true,
-				-- show_on_insert = true,
-			},
-			sources = {
-				default = { "lsp", "path", "snippets", "buffer" },
-			},
-			fuzzy = { implementation = "prefer_rust_with_warning" },
-		},
-		opts_extend = { "sources.default" },
-	},
-	{
+        dependencies = { "rafamadriz/friendly-snippets" },
+        version = "1.*",
+        opts = {
+            keymap = { preset = "default" },
+            appearance = {
+                nerd_font_variant = "mono",
+            },
+            completion = {
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 150,
+                },
+                ghost_text = {
+                    enabled = true,
+                },
+            },
+            signature = {
+                enabled = true,
+                -- show_on_insert = true,
+            },
+            sources = {
+                default = { "lsp", "path", "snippets", "buffer" },
+            },
+            fuzzy = { implementation = "prefer_rust_with_warning" },
+        },
+        opts_extend = { "sources.default" },
+    },
+    {
         -- Setup with :Copilot setup
         -- Get status with :Copilot status
-		"github/copilot.vim",
-	},
+        "github/copilot.vim",
+    },
+    {
+        -- automatically pair ", (, etc
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = function()
+            require("nvim-autopairs").setup({})
+        end,
+    },
+    {
+        -- automatically close and rename html tags
+        "windwp/nvim-ts-autotag",
+        event = "InsertEnter",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        config = function()
+            require("nvim-ts-autotag").setup()
+        end,
+    },
 }
