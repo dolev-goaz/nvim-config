9f8448302f7c3b8b01564745e1e55a81fec975d7
dolev <www.dolev@gmail.com>
2025-05-16 01:28:18 +0300

    use telescope builtins

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,62 +1,65 @@
 return {
-    {
-        "williamboman/mason-lspconfig.nvim",
-        event = "VeryLazy",
-        dependencies = { "williamboman/mason.nvim" },
-        config = function()
-            require("mason").setup({})
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "ts_ls", "volar" },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = {
-            "saghen/blink.cmp",
-            {
-                "folke/lazydev.nvim",
-                opts = {
-                    library = {
-                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-                    },
-                },
-            },
-        },
-        config = function()
-            local lspconfig = require("lspconfig")
-            local blink_capabilities = require("blink.cmp").get_lsp_capabilities()
+	{
+		"williamboman/mason-lspconfig.nvim",
+		event = "VeryLazy",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason").setup({})
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "ts_ls", "volar" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"saghen/blink.cmp",
+			{
+				"folke/lazydev.nvim",
+				opts = {
+					library = {
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+		},
+		config = function()
+			local lspconfig = require("lspconfig")
+			local blink_capabilities = require("blink.cmp").get_lsp_capabilities()
+			local telescope = require("telescope.builtin")
 
-            -- lua lsp
-            lspconfig["lua_ls"].setup({ capabilities = blink_capabilities })
+			-- lua lsp
+			lspconfig["lua_ls"].setup({ capabilities = blink_capabilities })
 
-            -- typescript lsp
-            local vue_language_server_path = vim.fn.stdpath("data")
-                .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
-            lspconfig["ts_ls"].setup({
-                capabilities = blink_capabilities,
-                init_options = {
-                    plugins = {
-                        {
-                            name = "@vue/typescript-plugin",
-                            location = vue_language_server_path,
-                            languages = { "vue" },
-                        },
-                    },
-                },
-                filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-            })
+			-- typescript lsp
+			local vue_language_server_path = vim.fn.stdpath("data")
+				.. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+			lspconfig["ts_ls"].setup({
+				capabilities = blink_capabilities,
+				init_options = {
+					plugins = {
+						{
+							name = "@vue/typescript-plugin",
+							location = vue_language_server_path,
+							languages = { "vue" },
+						},
+					},
+				},
+				filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+			})
 
-            -- vue lsp
-            lspconfig["volar"].setup({ capabilities = blink_capabilities })
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go To Definition" })
-            vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "Go To Declaration" })
-            vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, { desc = "Go To Implementation" })
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Go To References" })
+			-- vue lsp
+			lspconfig["volar"].setup({ capabilities = blink_capabilities })
 
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
-        end,
-    },
+			-- keymaps
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
+			vim.keymap.set("n", "<leader>gd", telescope.lsp_definitions, { desc = "Go To Definition" })
+			vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "Go To Declaration" })
+			vim.keymap.set("n", "<leader>gi", telescope.lsp_implementations, { desc = "Go To Implementation" })
+			vim.keymap.set("n", "<leader>gr", telescope.lsp_references, { desc = "Go To References" })
+
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
+		end,
+	},
 }
