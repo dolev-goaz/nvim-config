0073bc62d46aee97cc263ffaca515b93050f3d08
dolev <www.dolev@gmail.com>
2025-05-15 21:19:06 +0300

    diagnostics moved to separate file

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,77 +1,81 @@
 local function ensure_installed(package_name, installed_packages_names)
-    for _, value in pairs(installed_packages_names) do
-        if value == package_name then
-            return true
-        end
-    end
-    vim.notify(
-        string.format("Package %s is not installed. Please install it using Mason.", package_name),
-        vim.log.levels.WARN
-    )
-    return false
+	for _, value in pairs(installed_packages_names) do
+		if value == package_name then
+			return true
+		end
+	end
+	vim.notify(
+		string.format("Package %s is not installed. Please install it using Mason.", package_name),
+		vim.log.levels.WARN
+	)
+	return false
 end
 
 local null_ls_file_types = {
-    -- file types supported by prettier
-    "typescriptreact",
-    "javascriptreact",
-    "typescript",
-    "javascript",
-    "vue",
-    "json",
-    "css",
-    "scss",
-    "html",
+	-- file types supported by prettier
+	"typescriptreact",
+	"javascriptreact",
+	"typescript",
+	"javascript",
+	"vue",
+	"json",
+	"css",
+	"scss",
+	"html",
 
-    -- stylua
-    "lua",
+	-- stylua
+	"lua",
 }
 local function format_document()
-    vim.lsp.buf.format({
-        filter = function(client)
-            local ft = vim.bo.filetype
-            local use_null_ls = vim.tbl_contains(null_ls_file_types, ft)
-            if use_null_ls then
-                return client.name == "null-ls"
-            end
-            return true
-        end,
-    })
+	vim.lsp.buf.format({
+		filter = function(client)
+			local ft = vim.bo.filetype
+			local use_null_ls = vim.tbl_contains(null_ls_file_types, ft)
+			if use_null_ls then
+				return client.name == "null-ls"
+			end
+			return true
+		end,
+	})
 end
 
 return {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-        "nvimtools/none-ls-extras.nvim",
-    },
-    config = function()
-        local null_ls = require("null-ls")
-        local installed_packages = require("mason-registry").get_installed_package_names()
-        local sources = {}
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		local installed_packages = require("mason-registry").get_installed_package_names()
+		local sources = {}
 
-        -- NOTE: only install eslint_d version 13.1.2. In higher versions, it requires files
-        -- with the format eslint.config.js.
-        -- Install command: :MasonInstall eslint_d@13.1.2
-        -- Sources:
-        -- -- https://www.reddit.com/r/neovim/comments/1fdpap9/eslint_error_could_not_parse_linter_output_due_to/
-        -- -- https://eslint.org/docs/latest/use/migrate-to-9.0.0
-        if ensure_installed("eslint_d", installed_packages) then
-            table.insert(sources, require("none-ls.diagnostics.eslint_d"))
-            table.insert(sources, require("none-ls.code_actions.eslint_d"))
-        end
-        if ensure_installed("prettier", installed_packages) then
-            table.insert(sources, null_ls.builtins.formatting.prettier)
-        end
-        if ensure_installed("stylua", installed_packages) then
-            table.insert(sources, null_ls.builtins.formatting.stylua)
-        end
-        null_ls.setup({ sources = sources })
+		-- NOTE: only install eslint_d version 13.1.2. In higher versions, it requires files
+		-- with the format eslint.config.js.
+		-- Install command: :MasonInstall eslint_d@13.1.2
+		-- Sources:
+		-- -- https://www.reddit.com/r/neovim/comments/1fdpap9/eslint_error_could_not_parse_linter_output_due_to/
+		-- -- https://eslint.org/docs/latest/use/migrate-to-9.0.0
+		if ensure_installed("eslint_d", installed_packages) then
+			table.insert(sources, require("none-ls.diagnostics.eslint_d"))
+			table.insert(sources, require("none-ls.code_actions.eslint_d"))
+		end
+		if ensure_installed("prettier", installed_packages) then
+			table.insert(sources, null_ls.builtins.formatting.prettier)
+		end
+		if ensure_installed("stylua", installed_packages) then
+			table.insert(sources, null_ls.builtins.formatting.stylua)
+		end
+		null_ls.setup({ sources = sources })
 
-        -- formatting
-        vim.keymap.set("n", "<leader>cf", format_document, { desc = "Format current buffer" })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = { "*.ts", "*.js", "*.vue", "*.json", "*.css", "*.scss", "*.html", "*.lua" },
-            callback = format_document,
-        })
-    end,
+		-- Formatting
+		vim.keymap.set("n", "<leader>cf", format_document, { desc = "Format current buffer" })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = { "*.ts", "*.js", "*.vue", "*.json", "*.css", "*.scss", "*.html", "*.lua" },
+			callback = format_document,
+		})
+
+		-- Diagnostics
+		vim.keymap.set("n", "<leader>cd", require("telescope.builtin").diagnostics, { desc = "Open Diagnostics" })
+		vim.diagnostic.config({ virtual_text = true })
+	end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,38 +1,37 @@
 local function ensure_ripgrep()
-  local has_rg = vim.fn.executable("rg") == 1
-  if has_rg then return end
+    local has_rg = vim.fn.executable("rg") == 1
+    if has_rg then
+        return
+    end
 
-  vim.notify("ripgrep not found. On linux, try:\nsudo apt install ripgrep")
+    vim.notify("ripgrep not found. On linux, try:\nsudo apt install ripgrep")
 end
 ensure_ripgrep()
 
 return {
-  {
-  "nvim-telescope/telescope.nvim", tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Telescope: Search Git files" })
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep files" })
-    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find help" })
-
-    -- Diagnostics
-    vim.keymap.set("n", "<leader>td", builtin.diagnostics, { desc = "Telescope: All diagnostics" })
-    vim.diagnostic.config({ virtual_text = true })
-  end
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {}
-          }
-        }
-      })
-      require("telescope").load_extension("ui-select")
-    end
-  }
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Telescope: Search Git files" })
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep files" })
+            vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find help" })
+        end,
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+            require("telescope").setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                },
+            })
+            require("telescope").load_extension("ui-select")
+        end,
+    },
 }
