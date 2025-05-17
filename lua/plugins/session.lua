25d0ca304afe6c6ea16f66560325da156d71615c
dolev <www.dolev@gmail.com>
2025-05-17 18:37:29 +0300

    cleanup command

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -2,9 +2,9 @@ return {
 	"rmagatti/auto-session",
 	event = "VeryLazy",
 	keys = {
-		{ "<leader>sf", "<cmd>SessionSearch<CR>", desc = "[S]ession [F]ind" },
-		{ "<leader>sd", "<cmd>SessionDelete<CR>", desc = "[S]ession [D]elete" },
-		{ "<leader>sr", "<cmd>SessionRestore<CR>", desc = "[S]ession [R]estore" },
+		{ "<leader>sf", ":SessionSearch<CR>", desc = "[S]ession [F]ind" },
+		{ "<leader>sd", ":SessionDelete<CR>", desc = "[S]ession [D]elete" },
+		{ "<leader>sr", ":SessionRestore<CR>", desc = "[S]ession [R]estore" },
 		{ "<leader>sc", ":SessionSave ", desc = "[S]ession [C]reate" },
 	},
 	---@module "auto-session"
