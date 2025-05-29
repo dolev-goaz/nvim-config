64b8f1ab546f4e4be075c700a4a0625b73f04c3b
dolev <www.dolev@gmail.com>
2025-05-29 23:47:01 +0300

    cleanup declaration

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,24 +2,19 @@ return {
 	{
 		"lewis6991/gitsigns.nvim",
 		event = { "BufReadPost", "BufNewFile" },
-		config = function()
-			require("gitsigns").setup({})
-			vim.keymap.set("n", "<leader>ogp", ":Gitsigns preview_hunk<CR>", { desc = "[o]pen [g]it [p]review" })
-			vim.keymap.set(
-				"n",
-				"<leader>tgb",
-				":Gitsigns toggle_current_line_blame<CR>",
-				{ desc = "[t]oggle [g]it [b]lame" }
-			)
-			vim.keymap.set("n", "<leader>ogl", ":Telescope git_commits<CR>", { desc = "[o]pen [g]it [l]ogs" })
-			vim.keymap.set(
-				"n",
-				"<leader>oglb",
-				":Telescope git_bcommits<CR>",
-				{ desc = "[o]pen [g]it [l]ogs current [b]uffer" }
-			)
-			vim.keymap.set("n", "<leader>ogs", ":Telescope git_stash<CR>", { desc = "[o]pen [g]it [s]tash" })
-		end,
+		keys = {
+			{ "<leader>ogp", "<cmd>Gitsigns preview_hunk<CR>", desc = "[o]pen [g]it [p]review" },
+			{ "<leader>tgb", "<cmd>Gitsigns toggle_current_line_blame<CR>", desc = "[t]oggle [g]it [b]lame" },
+			{ "<leader>ogl", "<cmd>Telescope git_commits<CR>", desc = "[o]pen [g]it [l]ogs" },
+			{ "<leader>oglb", "<cmd>Telescope git_bcommits<CR>", desc = "[o]pen [g]it [l]ogs current [b]uffer" },
+			{ "<leader>ogs", "<cmd>Telescope git_stash<CR>", desc = "[o]pen [g]it [s]tash" },
+		},
+		opts = {
+			current_line_blame = true,
+			current_line_blame_opts = {
+				delay = 100,
+			},
+		},
 	},
 	{
 		"tpope/vim-fugitive",
