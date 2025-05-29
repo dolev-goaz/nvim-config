f723a3db1478599334683e08521cecdb6fcbd195
dolev <www.dolev@gmail.com>
2025-05-29 20:21:05 +0300

    load copilot only when entering a file

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -1,28 +1,28 @@
 local is_copilot_open = false
 local function toggle_copilot_chat()
-    local copilot_chat = require("CopilotChat")
-    if is_copilot_open then
-        copilot_chat.close()
-    else
-        copilot_chat.open({
-            context = { "buffer" },
-        })
-    end
-    is_copilot_open = not is_copilot_open
+	local copilot_chat = require("CopilotChat")
+	if is_copilot_open then
+		copilot_chat.close()
+	else
+		copilot_chat.open({
+			context = { "buffer" },
+		})
+	end
+	is_copilot_open = not is_copilot_open
 end
 
 return {
-    {
-        "CopilotC-Nvim/CopilotChat.nvim",
-        event = "VeryLazy",
-        dependencies = {
-            { "github/copilot.vim" },
-            { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
-        },
-        build = "make tiktoken",
-        config = function()
-            require("CopilotChat").setup({})
-            vim.keymap.set("n", "<C-c>", toggle_copilot_chat, { desc = "Toggle Copilot Chat" })
-        end,
-    },
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		event = { "BufReadPost", "BufNewFile" },
+		dependencies = {
+			{ "github/copilot.vim" },
+			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+		},
+		build = "make tiktoken",
+		config = function()
+			require("CopilotChat").setup({})
+			vim.keymap.set("n", "<C-c>", toggle_copilot_chat, { desc = "Toggle Copilot Chat" })
+		end,
+	},
 }
